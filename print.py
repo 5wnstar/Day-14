69b199d71f3b4f066eb0cfa86228cd8deeb20b33
Cyber-Jollof <53369822+Cyb3r-J0ll0f@users.noreply.github.com>
2021-09-17 12:28:45 +0100

    Update print.py

--- print.py
+++ print.py
@@ -1,1 +1,8 @@
+print("Hey!\nWelcome to Cyb3r_J0ll0f 30 days of codding")
 
+print(value(s), sep=' ', end='\n')
+
+
+#Here is an example of print() function that uses all the parameters as shown in its syntax given above:
+
+print("Hello", 123, "How are you?", sep="\n", end="\n", "This Cyb3r_J0ll0f")
